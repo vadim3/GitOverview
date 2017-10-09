3fd6f172f0b30f9f571c1dcd28fed7ee2fa05bee
Vadimas <popov@xakep.ru>
2017-10-09 21:25:39 +0300

    commit2

--- src/main/java/testPackage/Verty.java
+++ src/main/java/testPackage/Verty.java
@@ -4,5 +4,8 @@ package testPackage;
  * Created by Вадим on 09.10.2017.
  */
 public class Verty {
-    public static final
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
